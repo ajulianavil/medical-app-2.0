ec39d430a8ac30eed3a27ad9237b3d0df4972b09
ajulianavil <54120274+ajulianavil@users.noreply.github.com>
2023-09-25 11:59:47 -0500

    staticfiles

--- djang_website/settings.py
+++ djang_website/settings.py
@@ -166,10 +166,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR,'static')
 
+os.makedirs(STATIC_ROOT, exist_ok=True)
+os.makedirs(STATIC_TMP, exist_ok=True)
 STATICFILES_DIRS = [
-    BASE_DIR / "main/static",
+   os.path.join(BASE_DIR,'static')
 ]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "main/static",
+# ]
+
+# STATIC_ROOT = "/staticfiles/"
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'

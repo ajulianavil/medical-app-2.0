a1c3d1f52267fd9a80b864bdc0369921693a8cfe
ajulianavil <54120274+ajulianavil@users.noreply.github.com>
2023-09-25 10:48:05 -0500

    install of dotenv

--- djang_website/settings.py
+++ djang_website/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
